f632469dfd43f94eda519e5e653f8b582a433dbb
Nora Warschewski <93076262+norawarschewski@users.noreply.github.com>
2022-08-07 12:46:59 +0200

    fix: organise content by type

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,17 +7,17 @@ function setup(data) {
 
   for (let i = 0; i < data.length; i++) {
     const grepStyle = data[i];
-
+    console.log("null:", data[i].type)
     const description = document.createElement("div");
     description.setAttribute("class", 'grep-style');
 
     description.innerHTML += `
-    <div class="description">${grepStyle.description}</div>
-    <div class="grid__5-7"><code class="code">${grepStyle.find}</code></div>
-    <div class="grid__7-9"><code class="code">${grepStyle.replace}</code></div>
-    `;
+      <div class="description">${grepStyle.description}</div>
+      <div class="grid__5-7"><code class="code">${grepStyle.find}</code></div>
+      <div class="grid__7-9"><code class="code">${grepStyle.replace}</code></div>
+      `;
 
-    const parent = document.getElementById("grepStyles_common");
+    const parent = document.getElementById(`grepStyles_${data[i].type}`);
     parent.appendChild(description);
   }
 }
